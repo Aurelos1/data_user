08ca3452e531e5d683ebcc001f9b9dba58a67bcc
Aurelos1 <147404299+Aurelos1@users.noreply.github.com>
2024-04-25 16:56:36 +0200

    Inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserService } from './user.service';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  users: any[] = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.getUsers().subscribe(
+      (data: any[]) => {
+        this.users = data;
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des utilisateurs:', error);
+      }
+    );
+  }
   title = 'data_user';
+
+  downloadJson() {
+    const fileName = "users.json";
+    const json = JSON.stringify(this.users);
+    const blob = new Blob([json], { type: 'application/json' });
+    const href = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = href;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(href);
+  }
+  
 }
